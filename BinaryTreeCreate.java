91d4f198e76b18469973e0e1bedbf6a56b6186fb
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-21 04:55:01 +0000

    inorder traversal in tree

--- BinaryTreeCreate.java
+++ BinaryTreeCreate.java
@@ -12,10 +12,27 @@ public class BinaryTreeCreate {
 
          }
     }
+
+    // traversal on tree
+    // 1. inorder
+    // 2. preorder
+    // 3. post order
+
+    // inorder traversal
+    public static void inorder(Node root){
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+    }
+
     public static void main(String[] args) {
         Node root = new Node(10);
         root.left = new Node(20);
         root.right = new Node(30);
         root.left.left = new Node(40);
+
+        inorder(root);
     }
 }
