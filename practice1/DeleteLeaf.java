aa00830c07ce8e34699ec1a94332c6d2b0ae832f
mahfoozalamcse <amahfooz1007@gmail.com>
2025-09-06 13:02:04 +0530

    detete target value when they have leaf node

---
+++ practice1/DeleteLeaf.java
@@ -0,0 +1,54 @@
+package practice1;
+
+public class DeleteLeaf {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int value) {
+            data = value;
+            left = right = null;
+        }
+    }
+    static Node root;
+
+    // postorder + check target
+    static Node removeLeafNode(Node root, int target){
+       if (root == null) {
+        return null;
+       }
+
+       root.left = removeLeafNode(root.left, target);
+       root.right = removeLeafNode(root.right, target);
+ 
+       if (root.left == null && root.right == null && root.data == target) {
+          return null;
+       }
+       return root;
+    }
+
+    static void inorder(Node root){
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(" "+root.data);
+            inorder(root.right);
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(2);
+        root.right.left = new Node(2);
+        root.right.right = new Node(4);
+
+        System.out.println("Inorder before:");
+        inorder(root);
+        System.out.println();
+
+        root = removeLeafNode(root, 2);
+
+        System.out.println("Inorder after:");
+        inorder(root);
+    } 
+}
